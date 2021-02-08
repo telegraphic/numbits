467424c2dc9f5d24b13404e85019cac6d09b6ff6
Danny Price <dan@thetelegraphic.com>
2021-02-08 12:19:15 +0800

    Adding requant utility (needs testing)

--- src/numbits.cpp
+++ src/numbits.cpp
@@ -114,6 +114,91 @@ py::array_t<uint8_t> pack(py::array_t<uint8_t> inarray, int nbits)
   return outarray;
 }
 
+/* Function to rescale from complex 8-bit to 2-bit */
+py::array_t<uint8_t> requant_ci8_cu2(py::array_t<int8_t> input) {
+    // Setup pointers and buffers to access data in ndarray
+    py::buffer_info buf = input.request();
+
+    // Input array should have size 4x that of output array.
+    auto output = py::array_t<uint8_t>(buf.size / 4);
+    py::buffer_info bufo = output.request();
+    auto *v   = (int8_t *) buf.ptr;
+    auto *vo  = (uint8_t *) bufo.ptr;
+
+    // Compute STDEV for real and imag
+    double sum_re = 0.0,  sum_im = 0.0;
+    double sq_sum_re = 0.0,  sq_sum_im = 0.0;
+    size_t X = buf.shape[0] / 2;
+    for(size_t idx = 0; idx < X; idx++){
+        sum_re += v[2*idx];
+        sq_sum_re += v[2*idx] * v[2*idx];
+        sum_im += v[2*idx+1];
+        sq_sum_im += v[2*idx+1] * v[2*idx+1];
+    }
+    double mean_re = sum_re / X;
+    double mean_im = sum_im / X;
+    double stdev_re = std::sqrt(sq_sum_re / X - mean_re * mean_re);
+    double stdev_im = std::sqrt(sq_sum_im / X - mean_im * mean_im);
+
+    // Do 2-bit conversion
+    for(size_t idx = 0; idx < buf.shape[0] / 4; idx++) {
+
+        // We are going to add all 2-bits together into one 8-bit number
+        // So break out each into indexes
+        size_t idxr = 4*idx;
+        size_t idxi = 4*idx + 1;
+        size_t idxr2 = 4*idx + 2;
+        size_t idxi2 = 4*idx + 3;
+        //std::cout << v[idxr] << " " << v[idxi] << " ";
+
+        // Real part
+        if(v[idxr] <  -0.98159883*stdev_re) {
+            vo[idx] += 0 * 64;
+        } else if(v[idxr] < 0){
+            vo[idx] += 1 * 64;
+        } else if(v[idxr] < 0.98159883*stdev_re) {
+            vo[idx] += 2 * 64;
+        } else {
+            vo[idx] += 3 * 64;
+        }
+
+        if(v[idxr2] <  -0.98159883*stdev_re) {
+            vo[idx] += 0 * 4;
+        } else if(v[idxr2] < 0){
+            vo[idx] += 1 * 4;
+        } else if(v[idxr2] < 0.98159883*stdev_re) {
+            vo[idx] += 2 * 4;
+        } else {
+            vo[idx] += 3 * 4;
+        }
+
+        // Imag part
+        if(v[idxi] <  -0.98159883*stdev_im) {
+            vo[idx] += 0 * 16;
+        } else if(v[idxi] < 0) {
+            vo[idx] += 1 * 16;
+        } else if(v[idxi] < 0.98159883*stdev_im) {
+            vo[idx] += 2 * 16;
+        } else {
+            vo[idx] += 3 * 16;
+        }
+
+        if(v[idxi2] <  -0.98159883*stdev_im) {
+            vo[idx] += 0;
+        } else if(v[idxi2] < 0) {
+            vo[idx] += 1;
+        } else if(v[idxi2] < 0.98159883*stdev_im) {
+            vo[idx] += 2;
+        } else {
+            vo[idx] += 3;
+        }
+
+    }
+    return output;
+
+}
+
+
 PYBIND11_MODULE(numbits, m) {
     m.doc() = "Pack and unpack 1, 2 and 4 bit data"; // optional module docstring
 
@@ -121,4 +206,6 @@ PYBIND11_MODULE(numbits, m) {
           py::arg("inarray"), py::arg("nbits"));
     m.def("pack", &pack, "Pack 1, 2 and 4 bit data into an 8-bit numpy array",
          py::arg("inarray"), py::arg("nbits"));
+    m.def("requant_ci8_cu2", &requant_ci8_cu2, "Requantize 8-bit complex data into 2-bit complex data",
+         py::arg("inarray"));
 }
